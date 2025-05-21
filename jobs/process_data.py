68f0c2d93b9456320e226f4cd044a04b320a5f5c
Ibrahim <ibrahim.elaidouni@uit.ac.ma>
2025-05-21 21:18:59 +0100

    Modify *process-data.py* job To Enrich data with MAD Price Geting From API

--- jobs/process_data.py
+++ jobs/process_data.py
@@ -1,40 +1,57 @@
-from connect import create_spark_session, get_logger
-from data_io import read_json_from_minio, save_parquet_to_minio 
-from pyspark.sql.functions import col # type: ignore
-from config import MINIO_PARQUET_PATH , MINIO_JSON_PATH
+from jobs.connect import create_spark_session, get_logger
+from jobs.data_io import read_json_from_minio, save_parquet_to_minio, read_parquet_from_minio
+from pyspark.sql.functions import col, to_date, when
+from jobs.config import MINIO_PARQUET_PATH , MINIO_JSON_PATH, EXCHANGE_API_URL
+from jobs.scripts.rate import get_rate
+from pyspark.errors import AnalysisException
 
+# Initialize SparkSession with MinIO access
+logger = get_logger("Process Data")
+spark = create_spark_session("Extract and Process Crypto Data")
 
-def process_data(df):
-    flattened = df.select(
-        col("timestamp").cast("timestamp").alias("timestamp"),
-        col("bitcoin.usd").alias("bitcoin_usd"),
-        col("ethereum.usd").alias("ethereum_usd")
-    )
-    return flattened
+#                      ---------------------------------------------------
+# ----- Load Currency from MinIO & Get Rate Price USD/MAD ---------
+df = read_json_from_minio(spark, MINIO_JSON_PATH) 
 
-
-def main():
-    # Initialize SparkSession with MinIO access
-    logger = get_logger("Process Data")
-    spark = create_spark_session("Extract and Process Crypto Data")
-
-    # ----- Load JSON from MinIO ---------
-    df = read_json_from_minio(spark, MINIO_JSON_PATH)
-
-    # ----- Show schema and preview data ---------
-    # df.printSchema()
-    # df.show(5)
-
-    logger.info("--------------------- Starting data processing... -------------------------------")
-    
-    processed_data = process_data(df)
-    # processed_data.show(5)
-
-    save_parquet_to_minio(processed_data, MINIO_PARQUET_PATH)       # Saving Again To MINIO
-    logger.info("--------------------- Data Processing & Saving Done ----------------------------")
-
+try:
+    rate = get_rate(EXCHANGE_API_URL)
+    rate_date = rate["datetime"][:10]   #.split(" ")[0]      # e.g. "2025-05-21" without time and zone
+    rate_value = rate["rate"]
+except:
+    logger.info("Can't Fetch Rate")
     spark.stop()
 
+logger.info("---------------------Starting the Data Processing ----------------------------")
 
-if __name__ == "__main__":
-    main()
+flattened_df = df.select(
+    col("timestamp").cast("timestamp").alias("timestamp"),
+    col("bitcoin.usd").alias("BTC_usd"),
+    col("ethereum.usd").alias("ETH_usd")
+)
+
+# Apply Exchange Rate only if date matches
+enriched_df = flattened_df.withColumn(
+    "RATE",
+    when(to_date(col("timestamp")) == rate_date, rate_value)
+).withColumn(
+    "ETH_MAD",
+    when(to_date(col("timestamp")) == rate_date, col("ETH_usd") * rate_value)
+).withColumn(
+    "BTC_MAD",
+    when(to_date(col("timestamp")) == rate_date, col("BTC_usd") * rate_value)
+).withColumn(
+    "EXCH_time",
+    when(to_date(col("timestamp")) == rate_date, rate_date)
+)
+
+enriched_df.filter(to_date(col("timestamp")) == rate_date).show()
+# enriched_df.show()
+
+# Save all the Data to MINIO
+try:
+    save_parquet_to_minio(enriched_df, MINIO_PARQUET_PATH)       # Saving Again To MINIO
+    logger.info("--------------------- Data Processing & Saving Done ----------------------------")
+except AnalysisException as e:
+    logger.info(f"Can't Save To DATABASE {e}")
+
+spark.stop()
