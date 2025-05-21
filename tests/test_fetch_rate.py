c21418a747bcb7c1392fcfaa00152708be00776e
Ibrahim <ibrahim.elaidouni@uit.ac.ma>
2025-05-21 21:39:19 +0100

    Modify the test

--- tests/test_fetch_rate.py
+++ tests/test_fetch_rate.py
@@ -3,11 +3,14 @@ from jobs.config import EXCHANGE_API_URL
 
 def test_fetch_usd_mad():
     result = get_rate(EXCHANGE_API_URL)
-
+    
     assert isinstance(result, dict)
-    assert "USD/MAD" in result
+    assert "base/target" in result
     assert "rate" in result
     assert "datetime" in result
     assert "time_next_update" in result
-    assert isinstance(result["rate"], (int, float))
+    
+    assert result["base/target"] == "USD/MAD"
+    assert isinstance(result["rate"], float)
     assert isinstance(result["datetime"], str)
+    assert isinstance(result["time_next_update"], str)
